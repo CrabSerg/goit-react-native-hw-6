5f6ec075e495212f5675afb8ad89c184b0531c41
Serg <serg212007@gmail.com>
2023-02-26 22:37:13 +0200

    nex

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,13 +8,13 @@ import {
 import { getFirestore } from "firebase/firestore";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDAcDSgb8QfqQHnp_fr6wBZXv3QL_g63eQ",
-  authDomain: "newproduct-544a3.firebaseapp.com",
-  databaseURL: "https://newproduct-544a3-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "newproduct-544a3",
-  storageBucket: "newproduct-544a3.appspot.com",
-  messagingSenderId: "939687056981",
-  appId: "1:939687056981:web:5d3da288f18d92d702a6dc"
+  apiKey: "AIzaSyBBG6UT6Bc9CuHEmqJJoJy7b4vbqk445IU",
+  authDomain: "fir-20dc4.firebaseapp.com",
+  projectId: "fir-20dc4",
+  storageBucket: "fir-20dc4.appspot.com",
+  messagingSenderId: "1067126844759",
+  appId: "1:1067126844759:web:4cae5193021def9d13a212",
+  measurementId: "G-7M23EHSQG0"
 };
 
 // Initialize Firebase
